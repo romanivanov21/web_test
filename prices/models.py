791221976ffb7ea8f9dd0b74b3be5c51882a7534
Ivanov Roman <romanivanov21@mail.ru>
2015-09-26 22:28:24 +0300

    Create database tables

--- prices/models.py
+++ prices/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Category( models.Model ) : 
+	slug  =  models.SlugField( unique = True )
+	name  =  models.TextField()
+
+class Tag( models.Model ) :
+	slug  =  models.SlugField( unique = True )
+	name  =  models.TextField( unique = True )
+
+class Unit( models.Model ) :
+	name  =  models.TextField()
+
+class Lang( models.Model ) :
+	code  =  models.CharField( max_length = 3, primary_key = True)
+	name  =  models.TextField()
+
+class Good( models.Model ) :
+	slug  =  models.SlugField( unique = True )
+	name  =  models.TextField()
+	price =  models.DecimalField( max_digits = 22, decimal_places = 2 )
+	unit  =  models.ForeignKey( Unit )
+	cath  =  models.ForeignKey( Category )
+	tags  =  models.ManyToManyField( Tag )
+	langs =  models.ManyToManyField( Lang )
+
+
